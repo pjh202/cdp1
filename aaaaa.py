52bf23dd404761db398c03279f12e216587080b5
pjh202 <momdpjh@gmail.com>
2025-06-12 20:50:28 +0900

    Add files via upload

---
+++ aaaaa.py
@@ -0,0 +1,76 @@
+##def number(n) :
+##    if n % 2 == 0 :
+##        print('even')
+##    else :
+##        print('odd')
+##    return 0
+##---
+##
+##n =int(input())
+##if n % 2 == 0 :
+##    print('even')
+##else :
+##    print('odd')
+##s = 5
+##print(s) 
+##
+
+##def square(x):
+##    print(x**y)
+##
+##x = int(input())
+##y = int(input())
+##
+##square(x)
+
+##def season(x) :
+##    if  3<= x <= 5:
+##        return 'spring'
+##    
+##    elif 6<= x <= 8:
+##        return 'summer'
+##    elif  9<= x <= 11 :
+##        return 'fall'
+##    elif  0<x<= 2 or x==12:
+##        return 'winter'
+##
+##x = int(input())
+##print(season
+##      (x))
+##    
+
+##from random import*
+##
+##def lotto() :
+##    lot = set()
+##
+##    for i in range (6) :
+##        lot.add(randint(1, 45))
+##
+##    lot = list(lot)
+##    lot.sort()
+##    print(lot)
+##
+##lotto()
+
+##def f(n) :
+##    n - (n-1) = a
+##    a
+    
+    
+
+
+
+##n = int(input())
+##f(n)
+
+
+def func_abs(n):
+
+    s = n * (-1)
+    return s
+
+n = int(input())
+print(func_abs(n))
+    
+
